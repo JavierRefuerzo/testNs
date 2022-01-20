1defd46cf3757739a9cd10428a74136a1137b53b
Javier Refuerzo <javierrefuerzo@gmail.com>
2022-01-19 21:26:36 -0700

    test

--- nodes/controller/controller.py
+++ nodes/controller/controller.py
@@ -188,6 +188,7 @@ class Controller(udi_interface.Node):
 
             #do not parse type params
             enum = Params.get(value=param)
+            LOGGER.info("enum is: " + str(enum))
             if enum == None:
                 LOGGER.info("Not parsing IR Param is: " + param)
                 continue
