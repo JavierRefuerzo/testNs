4a1dd4a32e9cd12c01578eee8391d3b3505781c2
Javier Refuerzo <javierrefuerzo@gmail.com>
2022-01-19 19:32:34 -0700

    test

--- iTachLib/controller/codeSetParser.py
+++ iTachLib/controller/codeSetParser.py
@@ -41,51 +41,50 @@ class CodeSetParser:
     
         
         for index, code in enumerate(codes):
-            code = code.strip()
-            #check for empty strings
-            if not code:
-                print ("index Empty: " + str(index))
+
+            try:
+                code = code.strip()
+                #check for empty strings
+                if not code:
+                    print ("index Empty: " + str(index))
+                    continue
+                
+                #replace any blank items in the list as it will crash string literal comnversion
+                code = code.replace(",,",",").replace(",,",",").replace(",,",",")
+
+                #make code into a string literal list
+                code = "[" + code + "]"
+
+                #replace any empty strings at the end of the list i.e "XX","XX",]
+                code = code.replace(",]","]")
+
+                print("codeInfo is:\n" + code + "\n")
+
+                codeInfo = json.loads(code)
+                size = len(codeInfo)
+
+                # we need at least a function name
+                if size < 2:
+                    print("code size too small")
+                    continue
+                
+                print("Code: " + str(index) + ". len: " + str(size))
+                print("function: " + codeInfo[0])
+                print("code1 " + codeInfo[1])
+
+                ir = IrCode(button=codeInfo[0], gcCodeOne=codeInfo[1])
+                # if size > 2:
+                #     print("hexcode1 " + codeInfo[2])
+                if size > 3:
+                    print("hexcode1 " + codeInfo[3])
+                    ir.gdCodeTwo = codeInfo[3]
+                # if size > 4:
+                #     print("hexcode2 " + codeInfo[4])
+                
+            
+                #print("codeInfo is:\n" + codeInfo + "\n")
+
+                #print("index" + str(index) + "\n: " + code + "\n")
+            except Exception as e:
+                print(e)
                 continue
-            
-            #replace any blank items in the list as it will crash string literal comnversion
-            code = code.replace(",,",",").replace(",,",",").replace(",,",",")
-
-            #make code into a string literal list
-            code = "[" + code + "]"
-
-            #replace any empty strings at the end of the list i.e "XX","XX",]
-            code = code.replace(",]","]")
-
-            # codeInfo:List = ast.literal_eval(code)
-            # size = len(codeInfo)
-
-            print("codeInfo is:\n" + code + "\n")
-
-            # for string in codeInfo:
-            #     print(string)
-
-            codeInfo = json.loads(code)
-            size = len(codeInfo)
-
-            # we need at least a function name
-            if size < 2:
-                print("code size too small")
-                continue
-            
-            print("Code: " + str(index) + ". len: " + str(size))
-            print("function: " + codeInfo[0])
-            print("code1 " + codeInfo[1])
-
-            ir = IrCode(button=codeInfo[0], gcCodeOne=codeInfo[1])
-            # if size > 2:
-            #     print("hexcode1 " + codeInfo[2])
-            if size > 3:
-                print("hexcode1 " + codeInfo[3])
-                ir.gdCodeTwo = codeInfo[3]
-            # if size > 4:
-            #     print("hexcode2 " + codeInfo[4])
-            
-           
-            #print("codeInfo is:\n" + codeInfo + "\n")
-
-            #print("index" + str(index) + "\n: " + code + "\n")
