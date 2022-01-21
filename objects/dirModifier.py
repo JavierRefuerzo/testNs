c484fd88827d49396c78e41d82c120d035935117
Javier Refuerzo <javierrefuerzo@gmail.com>
2022-01-21 14:28:57 -0700

    test

--- objects/dirModifier.py
+++ objects/dirModifier.py
@@ -16,23 +16,28 @@ from iTachLib.controller.Device import Device
 
 class DirModifier :
     
+    devices: List[Device]
 
     def __init__(self, devices: List[Device]):
-
+        self.devices = devices
         self.makeNls()
-
 
     def makeNls(self):
         # There is only one nls, so read the nls template and write the new one
         en_us_txt = "profile/nls/en_test.txt"
         self.make_file_dir(en_us_txt)
         nls = open(en_us_txt,  "w")
-        test = 0
-        while test < 10:
-            nls.write("This is a test\n")
-            test = test + 1
-
         nls.write(DefaultNls.nls)
+        for index, device in enumerate(self.devices):
+            nls.write("#Device - " + device.name + "\n")    
+            for code in device.buttons:
+                #This should be changed to nodeAddress
+                name = self.get_valid_node_name(device.name)
+                command = name + "-" + str(index) + "\n"
+                nls.write(command)    
+            # add double line between commands
+            nls.write("\n\n")
+
         nls.close()
 
 
@@ -41,3 +46,13 @@ class DirModifier :
         if not os.path.exists(directory):
             os.makedirs(directory)
         return True
+
+    def get_valid_node_name(self, name, max_length=14) -> str:
+        offset = max_length * -1
+        # Only allow utf-8 characters
+        #  https://stackoverflow.com/questions/26541968/delete-every-non-utf-8-symbols-froms-string
+        name = bytes(name, 'utf-8').decode('utf-8','ignore')
+        # Remove <>`~!@#$%^&*(){}[]?/\;:"'` characters from name
+        sname = re.sub(r"[<>`~!@#$%^&*(){}[\]?/\\;:\"']+", "", name)
+        # And return last part of name of over max_length
+        return sname[offset:].lower()
